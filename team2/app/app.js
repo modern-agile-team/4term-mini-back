6f156214923d6c7b24e12538f1fc1188996ec496
JUHA <khabh@naver.com>
2022-07-26 13:33:06 +0900

    express로 서버 가동

--- team2/app/app.js
+++ team2/app/app.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/src/views`));
+app.use(express.urlencoded({ extended: true }));
+
+// const post = require("./src/apis/post");
+// const comment = require("./src/apis/comment");
+
+// app.use("/moae/post", post);
+// app.use("/moae/comment", comment);
+
+module.exports = app;
