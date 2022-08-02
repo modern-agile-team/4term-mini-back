8d2a26db4e099e9d67ec35daa8a80837266a0ce9
Kim minho <90795904+klaus9267@users.noreply.github.com>
2022-08-02 14:07:07 +0900

    feat: login경로 수정 #3

--- team1/app/src/apis/user/index.js
+++ team1/app/src/apis/user/index.js
@@ -8,7 +8,7 @@ const userCtrl = require("./user.Ctrl");
 
 router.get("/search/:nickname", authUtil, userCtrl.process.searchUser);
 
-router.post("/", userCtrl.process.login);
+router.post("/login", userCtrl.process.login);
 router.post("/register", userCtrl.process.register);
 router.delete("/:userNo", authUtil, userCtrl.process.delUser);
 router.patch("/profile/:userNo", authUtil, userCtrl.process.updateUser);
