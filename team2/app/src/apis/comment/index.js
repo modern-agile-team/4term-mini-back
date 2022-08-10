224451ae22b23a79b427db6822249f5fc0ad860b
JUHA <khabh@naver.com>
2022-08-10 14:04:01 +0900

    refactor: url 설정 변경 & 미들웨어 등록

--- team2/app/src/apis/comment/index.js
+++ team2/app/src/apis/comment/index.js
@@ -4,14 +4,17 @@ const express = require("express");
 const router = express.Router();
 const commentCtrl = require("./comment.Ctrl");
 const checkToken = require("../../middlewares/identify-auth");
-// checkToken.check.token
 
-router.post("/", commentCtrl.process.createComment);
+// router.post("/", commentCtrl.process.createComment);
+// router.delete("/", commentCtrl.process.deleteComment);
 
-// router.post("/", checkToken.check.token, commentCtrl.process.createComment);
-// router.patch("/:userNo", checkToken.check.token, commentCtrl.process.updateComment);
+router.post("/", checkToken.check.token, commentCtrl.process.createComment);
+router.patch(
+  "/:userNo",
+  checkToken.check.token,
+  commentCtrl.process.updateComment
+);
 router.get("/:postNo", commentCtrl.process.readComments);
-// router.delete("/", checkToken.check.token, commentCtrl.process.deleteComment);
-router.delete("/", commentCtrl.process.deleteComment);
+router.delete("/", checkToken.check.token, commentCtrl.process.deleteComment);
 
 module.exports = router;
