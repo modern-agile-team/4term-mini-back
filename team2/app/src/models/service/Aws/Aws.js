3db63a549aa82b1886f22493e1e343800fad3184
KimSoo <ilppang@naver.com>
2022-08-10 13:58:26 +0900

    feat: env변수명 변경 #3

--- team2/app/src/models/service/Aws/Aws.js
+++ team2/app/src/models/service/Aws/Aws.js
@@ -4,8 +4,8 @@ const aws = require("aws-sdk");
 require("dotenv").config();
 
 const s3 = new aws.S3({
-  accessKeyId: process.env.S3_KEYID,
-  secretAccessKey: process.env.S3_PRIVATE_KEY,
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_KEY,
   region: process.env.REGION,
 });
 
@@ -13,7 +13,7 @@ let upload = multer({
   //업로드
   storage: multerS3({
     s3,
-    bucket: process.env.S3_BUCKET_NAME,
+    bucket: process.env.AWS_BUCKET_NAME,
     contentType: multerS3.AUTO_CONTENT_TYPE,
     acl: "public-read",
     key: (req, file, cb) => {
@@ -26,6 +26,6 @@ exports.upload = multer(upload);
 exports.deleteProfile = async (req, res, next) => {
   //삭제
   const { userNo } = req.params;
-  await s3.deleteObject({ bucket: process.env.S3_BUCKET_NAME, key: `userNo${userNo}` });
+  await s3.deleteObject({ bucket: process.env.AWS_BUCKET_NAME, key: `userNo${userNo}` });
   next();
 };
